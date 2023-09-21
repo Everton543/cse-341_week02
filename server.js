41e421cfafc02b0a03c68200f79575c2bc84495a
Everton Alves <64697974+Everton543@users.noreply.github.com>
2023-09-20 22:53:22 -0300

    Part 1 done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 8080;
+const cors = require('cors');
+
+app.use(cors());
 app.use('/', require('./routes'));
 
 app.listen(port, () => {
